e9e21e3abe1f5c5fa121345053f2556284aefe50
Alejandro Toledo <atoledofr@gmail.com>
2024-05-06 15:57:40 -0500

    add users and jwt authentication

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < ActionController::API
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordNotDestroyed, with: :render_not_destroyed_response
+
+  private
+
+  def render_not_found_response(err)
+    render json: { error: err }, status: :not_found
+  end
+
+  def render_not_destroyed_response(err)
+    render json: { errors: err }, status: :unprocessable_entity
+  end
+end

dcc927d6de7d3fdbdfc4671f858a796890165786
Alejandro Toledo <atoledofr@gmail.com>
2024-05-06 18:41:10 -0500

    add validation for models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,8 @@
 class Book < ApplicationRecord
   belongs_to :author
+
+  validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 2, maximum: 255 }
+  validates :published_year, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: Time.now.year }, allow_nil: true
+
+  validates :author_id, presence: true
 end

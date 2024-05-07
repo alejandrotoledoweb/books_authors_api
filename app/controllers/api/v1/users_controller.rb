59f71e8e842a22993a1fda69ebd78133914bbd79
Alejandro Toledo <atoledofr@gmail.com>
2024-05-06 19:28:03 -0500

    fix user creation method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      include ActionController::HttpAuthentication::Token
+      before_action :authenticate_user, only: %i[create]
 
       def create
         user = User.new(user_params)
@@ -13,8 +15,16 @@ module Api
 
       private
 
+      def authenticate_user
+        token, _options = token_and_options(request)
+        user_id = AuthenticationTokenService.decode(token)
+        User.find(user_id)
+      rescue ActiveRecord::RecordNotFound, JWT::DecodeError
+        render json: { error: 'Unauthorized' }, status: :unauthorized
+      end
+
       def user_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:username, :password, :password_confirmation)
       end
     end
   end
